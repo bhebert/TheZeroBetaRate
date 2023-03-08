dcba4bf8098415b7967ffe5df0d166f9faad6535
Benjamin Hebert <bhebert@users.noreply.github.com>
2023-03-08 15:06:35 -0800

    Create InstrumentGMMConcOpt.m
    initial check-in

---
+++ MatlabCode/InstrumentGMMConcOpt.m
@@ -0,0 +1,19 @@
+function [obj] = InstrumentGMMConcOpt(Theta, R, Rm, Z, Rb, iotaN, iotaM, ConsG,inflation,Rfex,asset,NLConsFactor, varargin)
+%wrapper function to compute GMM objective
+T = size(R, 2);
+
+if strcmp(asset,'ZB')
+    [Beta, Sigma] = BetaSigma(R, Rm, Z, Rb, ConsG, inflation,iotaN, iotaM, 0, zeros(K,1), sigma,NLConsFactor);
+    weight =  PortfolioWeight(Beta,Sigma,iotaN);
+    Nm = size(Z,1) + 1;
+else
+    Beta = [];
+    weight = [];
+    Nm = 1;
+end
+
+mmoments = InstMomentsConc(Theta, Beta, weight, R, Rm, Z, Rb, iotaN, iotaM, ConsG,inflation,Rfex, asset,NLConsFactor);
+
+obj = T*mmoments(1:Nm)'* mmoments(1:Nm);
+
+
