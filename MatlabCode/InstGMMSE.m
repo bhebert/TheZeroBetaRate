36331271186d4e94e908e9ff6393515cf00c76e4
Benjamin Hebert <bhebert@users.noreply.github.com>
2023-03-08 15:28:01 -0800

    initial check-in
    updated to new format for InstMomentsConc

---
+++ MatlabCode/InstGMMSE.m
@@ -0,0 +1,55 @@
+function [pvar,mvar] = InstGMMSE(Theta,Beta,alphas, R, Rm, Z, Rb, iotaN, iotaM, ConsG,inflation,weight,har,sig,NLConsFactor,varargin)
+
+    K = size(Z, 1);
+    T= size(Z,2);
+
+    if strcmp(asset,'ZB')
+        [~,amoments, cmoments, projmoments] = InstMomentsConc([Theta1;sig],Beta,weight, Rinput, Rminput, Zinput, Rbinput, iotaN, iotaM, ConsG,inflation,Rfex,'ZBFull',NLConsFactor);
+        momfull=[projmoments; amoments; cmoments];
+
+        %[~, momfull] = InstMomentsFull(Theta,Beta,alphas', R, Rm, Z, Rb, iotaN, iotaM, ConsG,inflation,weight,NLConsFactor);
+    end
+
+   
+
+    Nm = size(momfull,1);
+
+    %this is the exact identifiction weight matrix
+    Wmat = zeros(Nm);
+    Wmat(1:(Nm-K),1:(Nm-K)) = eye(Nm-K);
+
+
+    Omega = Wopt(momfull,har);
+
+
+    %the below commented code would compute the jacobian numerically
+    %we have instead written a function to compute the projection moment
+    %part of the jacobian analytically, which speeds things up
+    %we will compute the asset pricing/euler part numerically
+    %xinit = [Theta(1:end-1);reshape(Beta,size(Beta,1)*size(Beta,2),1);alphas'];
+
+%     func = @(x) InstMomentsFull([x(1:2+K);sig],reshape(x(3+K:2+K+size(Beta,1)*size(Beta,2)), ...
+%         size(Beta,1),size(Beta,2)),reshape(x(3+K+size(Beta,1)*size(Beta,2):end),size(Beta,1),1), ...
+%         R,Rm,Z,Rb,iotaN,iotaM,ConsG,inflation,weight);
+% 
+%     jac = jacobianest(func,xinit);
+    
+
+    jac = InstMomentsFullJacobian(Theta,Beta,alphas, R, Rm, Z, Rb, iotaN, iotaM, ConsG,inflation, weight, sig,NLConsFactor);
+
+
+
+    %the usual GMM parameter variance formula
+    GWGiGW = (jac'*Wmat*jac) \ (jac' * Wmat);   
+    pvar = 1/T* GWGiGW * Omega * (GWGiGW)';
+
+    %the usual GMM moment variance formula
+    mvar = 1/T * (eye(Nm) - jac*GWGiGW) * Omega * (eye(Nm) - jac*GWGiGW)'; 
+
+    %these are the only non-zero parts
+    mvar = mvar(Nm-K+1:end,Nm-K+1:end);
+
+    %only care about uncertainty with respect to non-alpha/beta parameters
+    pvar = pvar(1:K+2,1:K+2);
+
+end
---
+++ MatlabCode/InstMomentsFullJacobian.m
@@ -0,0 +1,34 @@
+function momjacbobian = InstMomentsFullJacobian(Theta,Beta,alphas, R, Rm, Z, Rb, iotaN, iotaM, ConsG,inflation, weight, sig,NLConsFactor,varargin)
+
+%  Computes the full jacobian of all moments
+%  Takes a mixed approach.
+%  For projections moments, the jacobian is computed analytically.
+%  For asset pricing and euler moments, the jacobian is computed numerically.
+
+    K = size(Z, 1);
+    
+
+    jacProj = InstMomentsProjectionJacobian(Theta,Beta,alphas, R, Rm, Z, Rb, iotaN, iotaM, ConsG,inflation,NLConsFactor);
+    
+    xstart = [Theta(1:end-1);reshape(Beta,size(Beta,1)*size(Beta,2),1);alphas'];
+    
+
+    %reference for what function call should look like
+    %use 'ZB' and not 'ZBFull' because we don't want proj moments
+    %okay if Rfex = [] because it won't be used
+    %InstMomentsConc([Theta1;sig],Beta,weight, Rinput, Rminput, Zinput, Rbinput, iotaN, iotaM, ConsG,inflation,Rfex,'ZB',NLConsFactor);
+
+    %alphas = reshape(x(3+K+size(Beta,1)*size(Beta,2):end),size(Beta,1),1),
+    %not necessary in function call
+
+    func = @(x) InstMomentsConc([x(1:2+K);sig],reshape(x(3+K:2+K+size(Beta,1)*size(Beta,2)), ...
+            size(Beta,1),size(Beta,2)), weight, ...
+            R,Rm,Z,Rb,iotaN,iotaM,ConsG,inflation,[],'ZB',NLConsFactor);
+
+    
+    jacAP = jacobianest(func, xstart);
+
+    momjacbobian = [jacProj; jacAP];
+
+end
+
