dd66690cef0967580d8e7c92b5d475690a7d2168
Benjamin Hebert <bhebert@users.noreply.github.com>
2023-03-09 10:06:56 -0800

    Update PortfolioWeight.m
    Fixes wrong dimension used in new file PortfolioWeight (created as part of code restructuring)

--- MatlabCode/PortfolioWeight.m
+++ MatlabCode/PortfolioWeight.m
@@ -3,7 +3,7 @@ function [weight] = PortfolioWeight(Beta,Sigma,iotaN)
 
     %construct zero-beta portfolio
     %todo: figure out way to compute with backslash operator to speed up
-    M = size(Beta,1);
+    M = size(Beta,2);
     Amat = [iotaN, Beta];
     Sinv = pinv(Sigma);
     e1 = [1; zeros(M,1)];
