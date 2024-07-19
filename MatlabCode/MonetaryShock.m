8d3efa98b4e94447e62b85caaeae5cb9d59ea923
Benjamin Hebert <bhebert@users.noreply.github.com>
2024-07-19 10:48:58 -0700

    Added option to include contemporaneous response
    this was asked for by a referee but not (yet) included in the paper

--- MatlabCode/MonetaryShock.m
+++ MatlabCode/MonetaryShock.m
@@ -6,6 +6,10 @@ close all;
 % visually distinct colors via colorbrewer2.org
 colors_list = {'#e41a1c','#377eb8', '#4daf4a','#984ea3','#ff7f00'};
 
+%flag for including contemporaneous result
+%0: don't use contemporaneous (WP version)
+%1: include comptemporaneous (suggested by referee)
+use_0 = 0;
 
 Nlags = 12;
 
@@ -39,11 +43,11 @@ writetable(DT2a,'../Output/NSData_Main.csv');
 %% Local Projections With RR
 REGDT = table2array(DT1(:, 2:end));
 reg_results = zeros(Nlags, 3, 3);
-for l = 0:Nlags-1
+for l = 0:Nlags-1+use_0
     % ---- zero beta rate ----
     % change
     %
-    mdl = fitlm(REGDT(2:end-l-1,6),12*(REGDT(l+3:end,1) - REGDT(1:end-2-l,1)));
+    mdl = fitlm(REGDT(2:end-l-1,6),12*(REGDT(l+3-use_0:end-use_0,1) - REGDT(1:end-2-l,1)));
 
     reg_results(1+l, 1, 1) = mdl.Coefficients.Estimate(2);
     CIs = coefCI(mdl);
@@ -51,7 +55,7 @@ for l = 0:Nlags-1
     reg_results(1+l, 3, 1) = CIs(2,2); 
 
     % ---- t-bill rate ----
-    mdl = fitlm(REGDT(2:end-l-1,6),12*(REGDT(l+3:end,2) - REGDT(1:end-2-l,2)));
+    mdl = fitlm(REGDT(2:end-l-1,6),12*(REGDT(l+3-use_0:end-use_0,2) - REGDT(1:end-2-l,2)));
 
     reg_results(1+l, 1, 2) = mdl.Coefficients.Estimate(2);
     CIs = coefCI(mdl);
@@ -59,7 +63,7 @@ for l = 0:Nlags-1
     reg_results(1+l, 3, 2) = CIs(2,2); 
 
     % ---- exp. cons ----
-    mdl = fitlm(REGDT(2:end-l-1,6),(REGDT(l+3:end,3) - REGDT(1:end-2-l,3)));
+    mdl = fitlm(REGDT(2:end-l-1,6),(REGDT(l+3-use_0:end-use_0,3) - REGDT(1:end-2-l,3)));
 
     reg_results(1+l, 1, 3) = mdl.Coefficients.Estimate(2);
     CIs = coefCI(mdl);
@@ -72,9 +76,9 @@ figure(1);
 
 hold on
 
-plot(1:Nlags, (reg_results(:, 1, 1))/5, '-','LineWidth',2,'Color',colors_list{1})
-plot(1:Nlags, (reg_results(:, 1, 2)), '-.','LineWidth',2,'Color',colors_list{3})
-plot(1:Nlags, (reg_results(:, 1, 3)), '--','LineWidth',2,'Color',colors_list{2})
+plot((1:Nlags+use_0)-use_0, (reg_results(:, 1, 1))/5, '-','LineWidth',2,'Color',colors_list{1})
+plot((1:Nlags+use_0)-use_0, (reg_results(:, 1, 2)), '-.','LineWidth',2,'Color',colors_list{3})
+plot((1:Nlags+use_0)-use_0, (reg_results(:, 1, 3)), '--','LineWidth',2,'Color',colors_list{2})
 
 set(gca,'TickLabelInterpreter','latex')
 legend('Real Zero-Beta Rate /5','$\bf{E}[$Real T-Bill Return$]$','$\bf{E}[$Cons. Growth$]$','show','Location','southwest','Interpreter','Latex')
@@ -90,10 +94,10 @@ saveas(gcf, "../Output/local_projection_rr.png")
 % Local Projections With NS
 REGDT = table2array(DT2(:, 2:end));
 reg_results = zeros(Nlags, 3, 2);
-for l = 0:Nlags-1
+for l = 0:Nlags-1+use_0
     % ---- zero beta rate ----
     % change
-    mdl = fitlm(REGDT(2:end-l-1,5),12*(REGDT(l+3:end,1) - REGDT(1:end-2-l,1)));
+    mdl = fitlm(REGDT(2:end-l-1,5),12*(REGDT(l+3-use_0:end-use_0,1) - REGDT(1:end-2-l,1)));
 
     reg_results(1+l, 1, 1) = mdl.Coefficients.Estimate(2);
     CIs = coefCI(mdl);
@@ -103,7 +107,7 @@ for l = 0:Nlags-1
     % ---- t-bill rate ----
 
 
-    mdl = fitlm(REGDT(2:end-l-1,5),12*(REGDT(l+3:end,2) - REGDT(1:end-2-l,2)));
+    mdl = fitlm(REGDT(2:end-l-1,5),12*(REGDT(l+3-use_0:end-use_0,2) - REGDT(1:end-2-l,2)));
 
 
     reg_results(1+l, 1, 2) = mdl.Coefficients.Estimate(2);
@@ -113,7 +117,7 @@ for l = 0:Nlags-1
 
 
         % ---- exp. cons ----
-    mdl = fitlm(REGDT(2:end-l-1,5),(REGDT(l+3:end,3) - REGDT(1:end-2-l,3)));
+    mdl = fitlm(REGDT(2:end-l-1,5),(REGDT(l+3-use_0:end-use_0,3) - REGDT(1:end-2-l,3)));
 
     reg_results(1+l, 1, 3) = mdl.Coefficients.Estimate(2);
     CIs = coefCI(mdl);
@@ -125,9 +129,9 @@ figure(2)
 
 
 hold on
-plot(1:Nlags, (reg_results(:, 1, 1))/5, '-', 'LineWidth',2,'Color',colors_list{1})
-plot(1:Nlags, (reg_results(:, 1, 2)), '-.', 'LineWidth',2,'Color',colors_list{3})
-plot(1:Nlags, (reg_results(:, 1, 3)), '--', 'LineWidth',2,'Color',colors_list{2})
+plot((1:Nlags+use_0)-use_0, (reg_results(:, 1, 1))/5, '-', 'LineWidth',2,'Color',colors_list{1})
+plot((1:Nlags+use_0)-use_0, (reg_results(:, 1, 2)), '-.', 'LineWidth',2,'Color',colors_list{3})
+plot((1:Nlags+use_0)-use_0, (reg_results(:, 1, 3)), '--', 'LineWidth',2,'Color',colors_list{2})
 ylim([-6,3])
 set(gca,'TickLabelInterpreter','latex')
 legend('Real Zero-Beta Rate /5','$\bf{E}[$Real T-Bill Return$]$','$\bf{E}[$Cons. Growth$]$','show','Location','southwest','Interpreter','Latex')
