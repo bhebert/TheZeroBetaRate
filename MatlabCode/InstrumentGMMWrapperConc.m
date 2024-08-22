f4a51e4f79046220e85ab4ccff48c6de7735290a
Benjamin Hebert <bhebert@users.noreply.github.com>
2024-08-21 21:50:56 -0700

    Add verification of convergence, global search if convergence fails

--- MatlabCode/InstrumentGMMWrapperConc.m
+++ MatlabCode/InstrumentGMMWrapperConc.m
@@ -15,6 +15,11 @@ rho_init = (0.08 - sig*0.015)/12;
 
 if strcmp(asset,'RF') || strcmp(asset,'Mkt')
     Theta0_init = [rho_init];
+    ub = 0.02;
+    lb = -ub;
+    TypX = 0.01;
+    %don't do global search if failing for some reason
+    cnt = 7;
 else
    
     % run once to build initial guess with zbrate = safe rate
@@ -24,21 +29,69 @@ else
 
     %use infeasible OLS as starting point
     Theta0 = [ones(1,size(Zinput,2)); Zinput]' \ (portRet_init-Rbinput)';
+
+    %repeat to iterate on a better guess
+    [Beta, Sigma] = BetaSigma(Rinput, Rminput, Zinput, Rbinput, ConsG, inflation,iotaN, iotaM, Theta0(1),reshape(Theta0(2:1+K), K, 1), sig,NLConsFactor,SigmaType);
+    weight =  PortfolioWeight(Beta,Sigma,iotaN);
+    portRet_init = weight * Rinput; 
+
+    Theta0 = [ones(1,size(Zinput,2)); Zinput]' \ (portRet_init-Rbinput)';
     Theta0_init = [Theta0; rho_init];
+
+    %bounds at large values to avoid singular matrices
+    ub = [ones(K+1,1)*3*max(abs(Theta0_init));.02];
+    lb = -ub;
+    TypX = [ones(K+1,1);0.01];
+    cnt=1;
 end
 
-
 %options_min = optimoptions('fminunc', 'Display','iter', 'MaxIterations', 1000,  'MaxFunctionEvaluations', 1e6,'OptimalityTolerance', 10^-8,'StepTolerance',10^-14,'FunctionTolerance',10^-10);
-options_min = optimoptions('fmincon', 'Display','off', 'MaxIterations', 10000,  'MaxFunctionEvaluations', 1e6,'OptimalityTolerance', 10^-8,'StepTolerance',10^-14,'FunctionTolerance',10^-10);
-
-
-
+options_min = optimoptions('fmincon', 'Display','off', 'MaxIterations', 100000,  'MaxFunctionEvaluations', 1e10,'OptimalityTolerance', 10^-10,'StepTolerance',10^-14,'FunctionTolerance',10^-10,'TypicalX',TypX);
 
 % GMM (psi>0: with ridge penalty)
 f1 = @(Theta) InstrumentGMMConcOpt([Theta;sig], Rinput, Rminput, Zinput, Rbinput, iotaN, iotaM, ConsG,inflation,Rfex,asset,NLConsFactor,SigmaType) + psi * sum(Theta(2:end-2).^2); 
 
 %[Theta1, ~]  = fminunc(f1, Theta0_init, options_min);
-[Theta1, ~]  = fmincon(f1, Theta0_init, [],[],[],[], -inf*ones(size(Theta0_init)),inf*ones(size(Theta0_init)),[], options_min);
+[Theta1, obj]  = fmincon(f1, Theta0_init, [],[],[],[], lb,ub,[], options_min);
+
+
+while (obj > 10^-6 && cnt < 6)
+    %try again
+    %use fixed seed
+    rng(12302018,'twister');
+    gs = GlobalSearch('FunctionTolerance',10^-10); %MultiStart('FunctionTolerance',10^-8,'UseParallel',true);
+
+    %iterate 10 times to move away from the stuck point
+    for i=1:10
+        [Beta, Sigma] = BetaSigma(Rinput, Rminput, Zinput, Rbinput, ConsG, inflation,iotaN, iotaM, Theta1(1), reshape(Theta1(2:1+K), K, 1), sig,NLConsFactor,SigmaType);
+        weight =  PortfolioWeight(Beta,Sigma,iotaN);
+        portRet_init = weight * Rinput; 
+
+        %use infeasible OLS as starting point
+        Theta0_1 = [ones(1,size(Zinput,2)); Zinput]' \ (portRet_init-Rbinput)';
+        Theta1 = [Theta0_1;Theta1(end)];
+    end
+
+    %try a relatively narrow region near this guess
+    ub = Theta1 + [0.02*ones(K+1,1);.001];
+    lb = Theta1 - [0.02*ones(K+1,1);.001];
+
+    problem = createOptimProblem('fmincon','x0',Theta1,'objective',f1,'lb',lb,'ub',ub); %'options',options_min
+    %'lb',lb,'ub',ub
+    
+    [Theta1, obj]  = run(gs,problem); % fmincon(f1, Theta0_init, [],[],[],[], -inf*ones(size(Theta0_init)),inf*ones(size(Theta0_init)),[], options_min2);
+    cnt = cnt + 1;
+    
+end
+
+if obj>10^-6
+
+    obj
+    Theta1
+    error('Numerical algorithm did not converge');
+end
+
+
 
 Theta1_final = [Theta1;sig];
 
