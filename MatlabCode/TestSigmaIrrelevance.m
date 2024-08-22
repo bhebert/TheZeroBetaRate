076874cc0f613d1c6e18e390c07c737004fec700
Benjamin Hebert <bhebert@users.noreply.github.com>
2024-08-21 21:40:04 -0700

    add statistics and swith to MktOnly
    MktOnly appears to have numerical issues, this helps with diagnosing the problem

--- MatlabCode/TestSigmaIrrelevance.m
+++ MatlabCode/TestSigmaIrrelevance.m
@@ -2,11 +2,38 @@
 %this is essentially a test of the numerical method
 
 clear;
-load("../Output/MainRun_Main.mat");
+load("../Output/MainRun_MktOnly.mat");
+opts.SigmaType="LW";
+addpath("../ExternalCode");
 
-[~, Theta2] = InstrumentGMMWrapperConc(Rinput, Rminput, Zinput, Rbinput, iotaN, iotaM, cons_gr_ann/12, inflation, Rfex, psi,2,'ZB',opts.har,opts.NLConsFactor,opts.SigmaType);
-[~, Theta10] = InstrumentGMMWrapperConc(Rinput, Rminput, Zinput, Rbinput, iotaN, iotaM, cons_gr_ann/12, inflation, Rfex, psi,10,'ZB',opts.har,opts.NLConsFactor,opts.SigmaType);
+warning('error','MATLAB:nearlySingularMatrix');
+
+[~, Theta2,~, portRet2,] = InstrumentGMMWrapperConc(Rinput, Rminput, Zinput, Rbinput, iotaN, iotaM, cons_gr_ann/12, inflation, Rfex, psi,2,'ZB',opts.har,opts.NLConsFactor,opts.SigmaType);
+[~, Theta10,~,portRet10] = InstrumentGMMWrapperConc(Rinput, Rminput, Zinput, Rbinput, iotaN, iotaM, cons_gr_ann/12, inflation, Rfex, psi,10,'ZB',opts.har,opts.NLConsFactor,opts.SigmaType);
 
 disp("the two vectors should be equal except for the last two params")
 disp("which are the discount rate and the sigma value")
 Theta2 - Theta10
+
+Rf2 = Theta2(1);
+gamma2 = reshape(Theta2(2:1+K), K, 1);
+
+Rf10 = Theta10(1);
+gamma10 = reshape(Theta10(2:1+K), K, 1);
+
+zbrate2 = gamma2'*Zinput + Rf2 + Rbinput; % zero-beta rate
+zbrate10 = gamma10'*Zinput + Rf10 + Rbinput; % zero-beta rate
+
+mean(portRet2-zbrate2)
+
+(portRet2-zbrate2)*(Rminput-iotaM*zbrate2)'/T
+
+([Zinput;ones(size(portRet3))]' \ (portRet2-Rbinput)')' - [gamma2;Rf2]'
+
+
+
+mean(portRet10-zbrate10)
+
+(portRet10-zbrate10)*(Rminput-iotaM*zbrate10)'/T
+
+([Zinput;ones(size(portRet3))]' \ (portRet10-Rbinput)')' - [gamma10;Rf10]'
