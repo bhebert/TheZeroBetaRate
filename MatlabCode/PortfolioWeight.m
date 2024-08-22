a363244666ad376c8b827d37c6b411a18eda02ca
Benjamin Hebert <bhebert@users.noreply.github.com>
2024-08-21 21:39:21 -0700

    remove pseudeo-inverse
    switch to inverse because pseudoinverse is unnecessary.

--- MatlabCode/PortfolioWeight.m
+++ MatlabCode/PortfolioWeight.m
@@ -1,14 +1,12 @@
 function [weight] = PortfolioWeight(Beta,Sigma,iotaN)
 
-
-    %construct zero-beta portfolio
-    %todo: figure out way to compute with backslash operator to speed up
-    M = size(Beta,2);
-    Amat = [iotaN, Beta];
-    Sinv = pinv(Sigma);
-    e1 = [1; zeros(M,1)];
-    mult = pinv(Amat' * Sinv * Amat) * e1;
-    weight = (Sinv* Amat * mult)';
-
-
+        %construct zero-beta portfolio
+        %todo: figure out way to compute with backslash operator to speed up
+        M = size(Beta,2);
+        Amat = [iotaN, Beta];
+        Sinv = inv(Sigma); %old: pinv
+        e1 = [1; zeros(M,1)];
+        mult = inv(Amat' * Sinv * Amat) * e1; %old: pinv
+        weight = (Sinv* Amat * mult)';
+  
 end
